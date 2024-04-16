c6b2d4e7bfc84fdd371434fca51fe675cad12c8d
Belkhodja Samy Mokhtar <57002921+Taldorus@users.noreply.github.com>
2024-04-16 08:53:14 +0100

    Add files via upload

---
+++ volumes/volumes/www/index.php
@@ -0,0 +1,19 @@
+<h1>Hello Cloudreach!</h1>
+<h4>Attempting MySQL connection from php...</h4>
+
+/*
+<?php
+    $servername = '192.168.157.129';
+    $dbname = 'mysql';
+    $username = 'dbuser';
+    $password = 'dbuserpassword';
+    try{
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname;port=3306", $username, $password);       
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo 'Connexion réussie';
+    }
+    catch(PDOException $e){
+        echo "Erreur : " . $e->getMessage();
+    }
+?>
+*/
